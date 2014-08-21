cf99057509fc9fb7798b6ddf7c8d971db5bcdec0
Alex Hudson <AlexHudson07@gmail.com>
2014-08-21 10:22:42 -0500

    first commit

---
+++ GitOn/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitOn
+//
+//  Created by Alex Hudson on 8/21/14.
+//  Copyright (c) 2014 Alex Hudson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitOn/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitOn
+//
+//  Created by Alex Hudson on 8/21/14.
+//  Copyright (c) 2014 Alex Hudson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
